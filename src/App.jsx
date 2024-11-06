223908affd77c68562cb51a3b06dea6ac9523c78
Juan Maldonado <juan.maldonado@live.cl>
2024-11-05 22:22:51 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Card from "./components/Card";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+
+      <Card />
+    </div>
+  );
+};
